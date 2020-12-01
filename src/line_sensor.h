431f9687f4259356b09d9c91db52a2fbdba2a0fd
Christos Soteriou <schris15@hotmail.com>
2020-12-01 11:33:47 +0000

    Modified PID Error Calculation

--- src/line_sensor.h
+++ src/line_sensor.h
@@ -6,14 +6,18 @@
 
 PID_s line_controller;
 
-#define LINE_DESIRED_POSITION 15
+#define LINE_DESIRED_POSITION 0
 
 #define LINE_CALIBRATION_SAMPLES 1000
-#define LINE_BLACK_ANALOG 700
+#define LINE_BLACK_MIN 700
 #define LINE_SENSOR_COUNT 4
 
-uint8_t line_sensors[] = {A0, A1, A2, A3};
-int16_t line_values[LINE_SENSOR_COUNT];
+#define LINE_SENSOR_MAX 1023
+#define LINE_SENSOR_MIN 0
+
+int8_t _line_sensor_weights[] = {-10, -5, 5, 10};
+uint8_t _line_sensors[] = {A0, A1, A2, A3};
+int16_t _line_values[LINE_SENSOR_COUNT];
 
 uint32_t _line_calib_black[LINE_SENSOR_COUNT];
 uint32_t _line_calib_white[LINE_SENSOR_COUNT];
@@ -22,7 +26,7 @@ void line_calibrate_black() {
     // Calibrate Black
     for (int i = 0; i < LINE_CALIBRATION_SAMPLES; i++) {
         for (int j = 0; j < LINE_SENSOR_COUNT; j++) {
-            _line_calib_black[j] += analogRead(line_sensors[j]); 
+            _line_calib_black[j] += analogRead(_line_sensors[j]); 
         }
     }
 
@@ -35,7 +39,7 @@ void line_calibrate_white() {
     // Calibrate Black
     for (int i = 0; i < LINE_CALIBRATION_SAMPLES; i++) {
         for (int j = 0; j < LINE_SENSOR_COUNT; j++) {
-            _line_calib_white[j] += analogRead(line_sensors[j]); 
+            _line_calib_white[j] += analogRead(_line_sensors[j]); 
         }
     }
 
@@ -44,7 +48,7 @@ void line_calibrate_white() {
     }
 }
 
-float line_error, line_position;
+float _line_error, _line_position_prev = 0;
 uint32_t t_update_line = 0;
 void line_update() {
     // Update Line Sensors Every 5ms
@@ -52,27 +56,33 @@ void line_update() {
         // Get Sensor Values
         // Switch from high@white -> high@black
         for (int i = 0; i < LINE_SENSOR_COUNT; i++) {
-            line_values[i] = 1023 - map(analogRead(line_sensors[i]), _line_calib_black[i], _line_calib_white[i], 0, 1023);
+            _line_values[i] = LINE_SENSOR_MAX - map(analogRead(_line_sensors[i]), _line_calib_black[i], _line_calib_white[i], LINE_SENSOR_MIN, LINE_SENSOR_MAX);
         }
 
         // Calculate Weighted Average 
+        float line_position = _line_position_prev;
         int32_t numerator = 0;
         int32_t denominator = 0;
         for (int i = 0; i < LINE_SENSOR_COUNT; i++) {
-            numerator += (i) * line_values[i];
-            denominator += line_values[i];
+            if (_line_values[i] > LINE_BLACK_MIN) {
+                numerator += _line_sensor_weights[i] * _line_values[i];
+                denominator += _line_values[i];
+            }
         }
-        line_position = ((float) numerator / (float) denominator) * 10.0;
+        if (denominator != 0) {
+            line_position = ((float) numerator / (float) denominator);
+            _line_position_prev = line_position;
+        }
 
         // Calculate position error
-        line_error = line_position - LINE_DESIRED_POSITION;
+        _line_error = line_position - LINE_DESIRED_POSITION;
 
         t_update_line = millis();
     }
 }
 
 void line_setup() {
-    PID_init(&line_controller, 10, 0, 0, &line_error);
+    PID_init(&line_controller, 40, 0, 0, &_line_error, 10e-3);
 }
 
 #endif
