7ef7943360e89ce35fc4d213ad3a02274b5c57bf
Christos Soteriou <schris15@hotmail.com>
2021-03-02 17:04:43 +0000

    working

--- master/main.cpp
+++ master/main.cpp
@@ -3,34 +3,58 @@
 #include <wiringPi.h>
 #include "pi2c.h"
 #include "ultrasonic.h"
+#include "pid.h"
 #include "EEEBot.h"
 #include "drive.h"
 
-ultrasonic_s dist;
-drive_s drive;
+#define Kp 10
+#define Ki 0
+#define Kd 0
+
+#define DIST_SETPOINT 10
+#define constrain(n, l, h) n < l ? l : n > h ? h : n
 
 int main(void) {
 	#ifdef DEBUG
 		printf("[+] Starting Setup\n");
 	#endif
-	
-	wiringPiSetupGpio();
 
+	uint32_t lastControl = 0;
+	ultrasonic_s dist;
+	drive_s drive;
+
+	PID_s pid;
+	
+	// Setup I2C & GPIO
+	wiringPiSetupGpio();
 	Pi2c i2c(EEEBOT_ADDR);
 
 	dist.i2c = &i2c;
 	drive.i2c = &i2c;
 
-	setMotors(&drive, 100, -100);
-
+	// Setup distance & drive
 	setContinuous(&dist, 1);
+	setMotors(&drive, 0, 0);
+
+	// Setup PID
+	float error = 0;
+	PID_init(&pid, Kp, Ki, Kd, &error);
 
 	#ifdef DEBUG
 		printf("[+] Starting Loop\n");
 	#endif
 
 	while (1) {
-		// getDistance(&dist);
+		if (millis() - lastControl > 100) {
+			getDistance(&dist);
+			error = DIST_SETPOINT - dist.distance;
+			float out = PID_compute(&pid, millis());
+			out = constrain(out, -255, 255);
+			
+			setMotors(&drive, out, out);
+
+			lastControl = millis();
+		}
 	}
 
 	return 1;
