a347841110f67ac26b44f0cb820e8dbcebd6b598
Christos Soteriou <schris15@hotmail.com>
2021-04-21 22:47:36 +0100

    contour detection

---
+++ inc/main.hpp
@@ -0,0 +1,14 @@
+#ifndef MAIN_HPP
+#define MAIN_HPP
+
+#include <stdio.h>
+#include <iostream>
+
+#include "opencv2/core.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+
+typedef std::vector<std::vector<cv::Point> > contours_t;
+typedef std::vector<cv::Vec4i> hierarchy_t;
+
+#endif /* MAIN_HPP */
--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,7 @@ Color Detector
 */
 
 // Include files for required libraries
-#include <stdio.h>
-#include <iostream>
-
-#include "opencv2/core.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp"
+#include "main.hpp"
 
 int main(int argc, char **argv)
 {
@@ -25,7 +20,7 @@ int main(int argc, char **argv)
     int gaussianSize = 11;    
     cv::createTrackbar("Gaussian Size", "Shape Detection Tester", &gaussianSize, 15, NULL);
     
-    int threshold1 = 150, threshold2 = 255;
+    int threshold1 = 172, threshold2 = 217;
     cv::createTrackbar("Threshold 1", "Shape Detection Tester", &threshold1, 300, NULL);
     cv::createTrackbar("Threshold 2", "Shape Detection Tester", &threshold2, 300, NULL);
 
@@ -35,15 +30,29 @@ int main(int argc, char **argv)
         threshold1      = cv::getTrackbarPos("Threshold 1", "Shape Detection Tester");
         threshold2      = cv::getTrackbarPos("Threshold 2", "Shape Detection Tester");
 
+        // Preprocess Image
         cv::Mat processed;
         cv::GaussianBlur(frame, processed, cv::Size(gaussianSize, gaussianSize), 0, 0);
         cv::cvtColor(processed, processed, cv::COLOR_BGR2GRAY);
 
+        // Find Edges
         cv::Canny(processed, processed, threshold1, threshold2, 3);
+
+        // Dilate Image
+        cv::Mat maskMorph = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5));
+        cv::dilate(processed, processed, maskMorph);
+
+        // Find Contours
+        contours_t contours;
+        hierarchy_t hierarchy;
+        cv::Mat draw_contour = frame.clone();
+        cv::findContours(processed, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_NONE);
+        cv::drawContours(draw_contour, contours, -1, (255, 255, 255), cv::LINE_8);
 
         cv::Mat combined, processed_bgr;
         cv::cvtColor(processed, processed_bgr, cv::COLOR_GRAY2BGR);
-        cv::hconcat(frame, processed_bgr, combined); 
+        cv::hconcat(frame, processed_bgr, combined);
+        cv::hconcat(combined, draw_contour, combined);
 
         cv::imshow("Shape Detection Tester", combined);
 
